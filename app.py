00d7b1b5cca2ddf8f55697b28935be5925ca4430
Precious <pcahaneku@gmail.com>
2025-08-20 20:13:24 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+        return render_template('homepage.html')
+
+@app.route('/registration')
+def registration():
+         return render_template('registrationpage.html')
+
+@app.route('/timetable')
+def timetable():
+         return render_template('timetable.html')
+
+if __name__ == '__main__':
+        app.run(debug=True)
